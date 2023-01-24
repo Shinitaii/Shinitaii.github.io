48a1af23e4fd6512d281429dd1e2855fd1d8829d
Shinn <sunikhedge@gmail.com>
2023-01-24 22:45:14 +0800

    removed bootstrap framework from the website, recoded (1/?)

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,4 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-
-const myButton = document.getElementById("btn-back-to-top");
-const about = document.getElementById("about-section");
-const skill = document.getElementById("skill-section");
-const career = document.getElementById("career-section");
-const outside = document.getElementById("outside-career-section");
-
-window.onscroll = function () {
-  scrollConditions(myButton, 110);
-  scrollConditionMiddle(about, 250, 850);
-  scrollConditionMiddle(skill, 500, 1500);
-  scrollConditions(career, 1250);
-  scrollConditions(outside, 1700);
-};
-
-
-
-function scrollConditions(variable, num1){
-  if(document.body.scrollTop > num1 || document.documentElement.scrollTop > num1){
-    ifTrue(variable);
-  } else {
-    ifFalse(variable);
-  }
-}
-
-function scrollConditionMiddle(variable, num1, num2){
-  if((document.body.scrollTop > num1 && document.body.scrollTop <= num2)  || (document.documentElement.scrollTop > num1 && document.documentElement.scrollTop < num2)){
-    ifTrue(variable);
-  } else {
-    ifFalse(variable);
-  }
-}
-
-function ifTrue(variable){
-  variable.style.display = "block";
-  variable.style.opacity = 1;
-  variable.style.visibility = "visible";
-  variable.style.animation = "fadeIn 0.5s";
-}
-
-function ifFalse(variable){
-  variable.style.animation = "fadeOut 0.6s";
-  variable.style.opacity = 0;
-  variable.style.visibility = "hidden";
-}
+const myButton = document.getElementById("back-to-top");
 
 myButton.addEventListener("click", backToTop);
 
@@ -53,3 +7,42 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+window.onscroll = function (){
+    scrollConditions(myButton, 20);
+}
+
+function scrollConditions(variable, num1){
+    if(document.body.scrollTop > num1 || document.documentElement.scrollTop > num1){
+      ifTrue(variable);
+    } else {
+      ifFalse(variable);
+    }
+}
+
+function ifTrue(variable){
+    variable.style.display = "block";
+    variable.style.opacity = 1;
+    variable.style.visibility = "visible";
+    variable.style.animation = "fadeIn 0.5s";
+  }
+  
+  function ifFalse(variable){
+    variable.style.animation = "fadeOut 0.6s";
+    variable.style.opacity = 0;
+    variable.style.visibility = "hidden";
+  }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+        if(entry.isIntersecting){
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
